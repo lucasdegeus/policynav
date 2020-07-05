b5df0deeefcb0a635bf0defa4c32c5e526984324
Lucas <lucasdegeus1999@gmail.com>
2020-07-05 22:41:54 +0200

    all files

---
+++ thesisCode/app.py
@@ -0,0 +1,304 @@
+import sys
+sys.path.append('/media/lucasdegeus/Storage/thesisCode/thesisCode/venv/lib/python3.6/site-packages')
+from flask import Flask, session, redirect, url_for, request, jsonify,abort
+from markupsafe import escape
+from werkzeug.utils import secure_filename
+from flask import render_template
+import numpy as np
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.snowball import EnglishStemmer
+from math import log10, sqrt
+import requests
+import justext
+from bs4 import BeautifulSoup
+import os
+import json
+
+nltk.download('punkt')
+nltk.download('stopwords')
+stemmer = EnglishStemmer()
+ps = PorterStemmer()
+
+pinterest = open("/home/thesis/myproject/policynav/thesisCode/policies/pinterest.txt",encoding="utf8").read()
+reddit = open("/home/thesis/myproject/policynav/thesisCode/policies/reddit.txt",encoding="utf8").read()
+linkedin = open("/home/thesis/myproject/policynav/thesisCode/policies/linkedin.txt",encoding="utf8").read()
+yahoo = open("/home/thesis/myproject/policynav/thesisCode/policies/yahoo.txt",encoding="utf8").read()
+spotify = open("/home/thesis/myproject/policynav/thesisCode/policies/spotify.txt",encoding="utf8").read()
+tumblr = open("/home/thesis/myproject/policynav/thesisCode/policies/tumblr.txt",encoding="utf8").read()
+snapchat = open("/home/thesis/myproject/policynav/thesisCode/policies/snapchat.txt",encoding="utf8").read()
+flickr = open("/home/thesis/myproject/policynav/thesisCode/policies/flickr.txt",encoding="utf8").read()
+telegram = open("/home/thesis/myproject/policynav/thesisCode/policies/telegram.txt",encoding="utf8").read()
+########################################################################
+whatsapp = open("/home/thesis/myproject/policynav/thesisCode/policies/whatsapp.txt",encoding="utf8").read()
+facebook = open("/home/thesis/myproject/policynav/thesisCode/policies/facebook.txt",encoding="utf8").read()
+twitter = open("/home/thesis/myproject/policynav/thesisCode/policies/twitter.txt",encoding="utf8").read()
+instagram = open("/home/thesis/myproject/policynav/thesisCode/policies/instagram.txt",encoding="utf8").read()
+google = open("/home/thesis/myproject/policynav/thesisCode/policies/google.txt",encoding="utf8").read()
+       
+def tokenizePolicy(textfile):
+    policy_token = [word.lower()  for word in word_tokenize(textfile)]
+    return(policy_token)
+
+def stemmedToken(textfile):
+    policy_token = [ps.stem(word.lower())  for word in word_tokenize(textfile)]
+    return(policy_token)
+
+def tokenizeTopic(topic):
+    tokenizedPolicies = []
+    for policy in topic:
+        tokenizedPolicies.append([ps.stem(word.lower())  for word in word_tokenize(policy) if not word in stopwords.words() and word.isalnum()])
+    return(tokenizedPolicies)
+
+def createList(policies):
+    totallist = []
+    for policy in policies:
+        totallist.append(policy)
+    return(totallist)
+
+def wordCounter(list, n):
+    wordOccurence = dict()
+    for policy in list:
+        for word in policy:
+            wordOccurence[word] = 0
+            for policy in list:
+                if word in policy:
+                    wordOccurence[word] += 1
+                wordOccurence[word] 
+    return(sorted(wordOccurence.items(), key=lambda x: x[1])[-n:])
+    
+def checker(words, policy):
+    word_index = dict()
+    for word in words:
+        index = np.where(np.array(policy) == word[0])[0]
+        word_index[word[0]] = index
+    return(word_index)    
+    
+    
+def findIndex(checker, policy):
+    indexcount = dict()
+    for value in checker:
+        occuredmost = []
+        for index in checker[value]:
+            counter=0
+            for othervalue in checker:
+                if value == othervalue:
+                    continue
+                else:
+                    if othervalue in policy[index-15:index+15]:
+                        counter+=1
+                        indexcount[index] = counter
+    return(indexcount)
+
+def maxIndex(indexes):
+    maxIndex = []
+    for item,value in indexes.items():
+        if value == indexes[max(indexes, key=indexes.get)]:
+            maxIndex.append(item)
+    return(maxIndex)
+
+   
+
+
+def removeDuplicates(indexes):
+    finalIndex = [indexes[0]]
+    for index in indexes:
+        for location in finalIndex:
+            if int(index) not in range(location-10,location+10):
+                if location == finalIndex[-1:]:
+                    finalIndex.append(index)
+                else:
+                    next
+            else:
+                break
+    return(finalIndex)
+
+def tf(policy,term):
+    try:
+        return(float(policy.count(term)))
+    except:
+        return(0)
+    
+def sortIndex(indexes, policy):
+    sortedIndex = dict()
+    for index in indexes:
+        sortedIndex[index] = tf(policy, policy[index])
+    return((sortedIndex))
+
+
+
+def ranker(sortedIndex, topicIndex, stemmedpolicy, wordcounted):
+    rankedWords = dict()
+    maximum = dict()
+    for item,value in sortedIndex.items():
+        for word in wordcounted:
+            if word[0] == stemmedpolicy[item]:
+                occurencesTraining = int(word[1])
+        rankedWords[item] = topicIndex[item]**2 / sortedIndex[item] * occurencesTraining
+    for item,value in rankedWords.items():
+        if value == rankedWords[max(rankedWords, key=rankedWords.get)]:
+            maximum[item] = value
+            print(maximum[item], item)
+    return(maximum)
+
+def minimumFrequency(sortedIndexes):
+    minimum = dict()
+    for item,value in sortedIndexes.items():
+        if value == sortedIndexes[min(sortedIndexes, key=sortedIndexes.get)]:
+            minimum[item] = value
+    return(minimum)
+
+def printContext(indexes, full_policy):
+    for index in indexes:
+        return(" ".join(full_policy[index-20:index+20]))
+        
+
+
+jsonfile = open('/home/thesis/myproject/policynav/thesisCode/json.json')
+jsonstr = jsonfile.read()
+def topicDefiner(topic):
+    if topic == 'datacontroller':
+        return(json.loads(jsonstr)[0])
+    if topic == 'purpose':
+        return(json.loads(jsonstr)[1])
+    if topic == 'legalbasis':
+        return(json.loads(jsonstr)[2])
+    if topic == 'recipients':
+        return(json.loads(jsonstr)[3])
+    if topic == 'retention':
+        return(json.loads(jsonstr)[4])
+    if topic == 'request':
+        return(json.loads(jsonstr)[5])
+    if topic == 'profiling':
+        return(json.loads(jsonstr)[6])
+    if topic == 'personaldata':
+        return(json.loads(jsonstr)[7])
+
+def returnTopIndex(topic, policy):
+    token_policy = tokenizePolicy(policy)
+    token_policy_stemmed = stemmedToken(policy)
+
+    wordsCounted = topic
+    checkedPolicy = checker(wordsCounted, token_policy_stemmed)
+    topicIndex = findIndex(checkedPolicy, token_policy_stemmed)
+    sortedIndex = sortIndex(topicIndex.keys(), token_policy)
+    rankedIndex = (ranker(sortedIndex,topicIndex, token_policy_stemmed, wordsCounted))
+
+    return(rankedIndex.keys())
+
+
+def returnCategories(policy):
+    try:
+        categorizedPolicy = tokenizePolicy(policy)
+
+        datacontroller = list(returnTopIndex(topicDefiner('datacontroller'), policy))
+        datacontroller.append('datacontroller')
+
+        purpose = list(returnTopIndex(topicDefiner('purpose'), policy))
+        purpose.append('purpose')
+
+        legalbasis = list(returnTopIndex(topicDefiner('legalbasis'), policy))
+        legalbasis.append('legalbasis')
+
+        recipients = list(returnTopIndex(topicDefiner('recipients'), policy))
+        recipients.append('recipients')
+
+        retention = list(returnTopIndex(topicDefiner('retention'), policy))
+        retention.append('retention')
+
+        request = list(returnTopIndex(topicDefiner('request'), policy))
+        request.append('request')
+
+        profiling = list(returnTopIndex(topicDefiner('profiling'), policy))
+        profiling.append('profiling')
+
+        personaldata = list(returnTopIndex(topicDefiner('personaldata'), policy))
+        personaldata.append('personaldata')
+
+        categories = [datacontroller, purpose, legalbasis, recipients, retention,request, profiling,personaldata ]
+        for category in categories:
+            value = category[-2]
+            try:
+                categorizedPolicy[value-15] = "<h1 id=' SECTION:" + category[-1] + " '>" + categorizedPolicy[value-15]
+            except:
+                categorizedPolicy[value] = "<h1 SECTION:" + category[-1] + " >" + categorizedPolicy[value]
+        print(categorizedPolicy[3023-25:3023])
+        return(" ".join(categorizedPolicy))
+    except:
+        return(False)
+
+
+
+def webScraper(url):
+    response = requests.get(url)
+    paragraphs = justext.justext(response.content, justext.get_stoplist('English'))
+    returningParagraphs = list()
+    for item in paragraphs:
+        returningParagraphs.append(item.text)
+    return(returningParagraphs)
+
+def exists(var):
+     var_exists = var in locals() or var in globals()
+     return var_exists
+names=['lucas', 'pieter' , 'geit']
+
+
+app = Flask(__name__)
+print("NewSession------------------")
+
+@app.errorhandler(500)
+def server_error(e):
+
+    email_admin(message="Server error", url=request.url, error=e)
+
+    app.logger.error(f"Server error: {request.url}")
+
+    return "Error ocured", 500
+
+
+@app.route('/about')
+def about():
+    return(render_template('about.html'))
+
+
+@app.route('/',  methods=['POST', 'GET'])
+def mainIndex():
+    if request.method == 'POST':
+        print("POSTER")
+        if 'file' not in request.files:
+            if 'urlInput' in request.form:
+                print("hi")
+                return(render_template('checktext.html', type='get', checkText=webScraper(request.form['urlInput'])))
+            else:
+                policy = request.form['description']
+                cleantext = BeautifulSoup(policy, "lxml").text
+                text = returnCategories(cleantext)
+                return(render_template('navigationpage.html', content=text.split('<h1')))
+    policies = []
+    for policy in os.listdir('/home/thesis/myproject/policynav/thesisCode/policies'):
+        policies.append(policy.split(".")[0])
+    return (render_template('index.html', topics=policies))
+
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    c = request.args.get('policy')
+    policycontent = open("/home/thesis/myproject/policynav/thesisCode/policies/" + c + ".txt",encoding="utf8").read()
+    return jsonify(policycontent)
+
+@app.route('/checkContent')
+def checkContent():
+    c = request.args.get('policy')
+    cleantext = BeautifulSoup(c, "lxml").text
+    text = returnCategories(cleantext)
+    if text == False:
+        return jsonify("False")
+    else:
+        return jsonify("True")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
